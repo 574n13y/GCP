a30c58ae94e17effe1a73db99dbbc1e21421585f
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-11-21 19:02:16 +0530

    Update main.py

--- Py for GCP/infra with py/main.py
+++ Py for GCP/infra with py/main.py
@@ -3,12 +3,10 @@ from network import *
 from compute import *
 from storage import *
 
-
 create_network(project,"app-vpc")
 create_subnetwork(project,"app-vpc","us-subnet",region,"10.10.0.0/28")
 create_gce_instance("test-1","us-central1-a","us-subnet","e2-medium",debian_image)
 create_bucket("bkt-dev-003","us")
-
 
 #To create multiple VMs
 for config in vm_configs:
